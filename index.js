f44882aa17fc2e056ca1877a2bad4a3c90c7199e
anilpirlaccbp <anilpirla224@gmail.com>
2023-09-27 05:21:08 +0000

    all test cases passed

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className="super-over-league-container">
+    <h1 className="heading">Super Over League</h1>
+    <div className="teams-image-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="team-logo"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="team-logo"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
